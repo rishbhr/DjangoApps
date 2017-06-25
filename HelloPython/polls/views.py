f25caf8d1d0ae9eb3ebb9a970e489145d01abca1
Rishbh Rana <rishbh@Rishbhs-Air.home>
2017-06-25 05:49:04 -0400

    build process is complete, but testing remains

--- HelloPython/polls/views.py
+++ HelloPython/polls/views.py
@@ -7,8 +7,25 @@ from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.db.models import F
+from django.views import generic
 
-from .models import Question
+from .models import Choice, Question
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		"""Return the last five published questions."""
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 # vote function
 def vote(request, question_id):
