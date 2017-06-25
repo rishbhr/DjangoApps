371931ea9b29431b65a7f060e2cfa9926f3a1d62
Rishbh Rana <rishbh@Rishbhs-Air.home>
2017-06-25 05:23:45 -0400

    addition of gui and functionality of voting

--- HelloPython/polls/views.py
+++ HelloPython/polls/views.py
@@ -1,10 +1,47 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.db.models import F
 
+from .models import Question
+
+# vote function
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Show voting form again
+		return render(request, 'polls/detail.html', { 
+			'question': question, 
+			'error_message': "You didn't select a choice.", 
+		})
+	else:
+		selected_choice.votes = F('votes') + 1
+		selected_choice.save()
+		# Return the POST data so the response is not recorded twice
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+# question page
 def index(request):
-	return HttpResponse("Hello, Jonathan. You're at the polls index.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+	question = get_object_or_404(Question, pk=question_id) 
+	return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." %question_id)
+
